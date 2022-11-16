9ab156b0665c32417ccd380dbe4349b7ee100f18
Szymon6699 <szymnow2112@gmail.com>
2022-11-16 15:17:15 +0100

    1

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+
+    <head> 
+
+        <body>
+
+            <?php
+            
+                $con = new msqli(127.0.0.1,"root","pass","ZSPSHOP");
+                
+            
+            ?>
+
+        </body>
+
+    </head>
+
+</html>
